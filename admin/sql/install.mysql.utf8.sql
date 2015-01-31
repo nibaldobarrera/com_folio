1d9171169ac2b438d5df0e4b10cc19bad3737f65
Nibaldo Barrera <nibaldobarrera@kazeta.cl>
2015-01-31 11:20:25 -0300

    V1.3.1 Se agrega SQL que faltaba
    Se agrega SQL que faltaba

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -8,6 +8,9 @@ CREATE TABLE IF NOT EXISTS `#__folio` (
   `company` varchar(250) NOT NULL DEFAULT '',
   `phone` varchar(12) NOT NULL DEFAULT '',
   `url` varchar(255) NOT NULL,
-  `description` TEXT, 
+  `description` TEXT,
+  `publish_up` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `publish_down` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `ordering` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
