bf03d632d507132df44266a5c0b66cd920cc723d
Nibaldo Barrera <nibaldobarrera@kazeta.cl>
2015-02-13 02:54:16 -0300

    V2.1.0 Se agregan imágenes demo propias
    Se agregan imágenes demo propias

--- script.php
+++ script.php
@@ -2,8 +2,33 @@
 defined('_JEXEC') or die;
 class com_folioInstallerScript
 {
+	function createSampleImages()
+	{
+		jimport( 'joomla.filesystem.file' );
+		if(!file_exists(JPATH_SITE . "/images/com_folio/"))
+		{
+			JFolder::create(JPATH_SITE. "/images/com_folio/");
+		};
+		if(!file_exists(JPATH_SITE. "/images/com_folio/index.html"))
+		{
+			JFile::copy(JPATH_SITE."/media/com_folio/index.html",JPATH_SITE. "/images/com_folio/index.html");
+		};
+		if(!file_exists(JPATH_SITE. "/images/com_folio/chocolate.png"))
+		{
+			JFile::copy(JPATH_SITE."/media/com_folio/images/chocolate. png",JPATH_SITE. "/images/com_folio/chocolate.png");
+		};
+		if(!file_exists(JPATH_SITE. "/images/com_folio/moneda.png"))
+		{
+			JFile::copy(JPATH_SITE."/media/com_folio/images/moneda. png",JPATH_SITE. "/images/com_folio/moneda.png");
+		};
+		if(!file_exists(JPATH_SITE. "/images/com_folio/galleta.png"))
+		{
+			JFile::copy(JPATH_SITE."/media/com_folio/images/glleta. png",JPATH_SITE. "/images/com_folio/galleta.png");
+		};
+	}
 	function install($parent)
 	{
+		$this->createSampleImages();
 		$parent->getParent()->setRedirectURL('index.php?option=com_folio');
 	}
 	function uninstall($parent)
@@ -12,6 +37,7 @@ class com_folioInstallerScript
 	}
 	function update($parent)
 	{
+		$this->createSampleImages();
 		echo '<p>' . JText::_('COM_FOLIO_UPDATE_TEXT') . '</p>';
 	}
 	function preflight($type, $parent)
