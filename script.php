03270459dd8a375aa701152ebd060019f8dce42d
Nibaldo Barrera <nibaldobarrera@kazeta.cl>
2015-02-15 01:19:50 -0300

    Corrige nombre de archivo
    Se corrige "monedas.jpg" por "moneda.jpg"

--- script.php
+++ script.php
@@ -4,48 +4,53 @@ class com_folioInstallerScript
 {
 	function createSampleImages()
 	{
-		jimport( 'joomla.filesystem.file' );
-		if(!file_exists(JPATH_SITE . "/images/com_folio/"))
+		jimport('joomla.filesystem.file');
+		if(!file_exists(JPATH_SITE."/images/com_folio/"))
 		{
-			JFolder::create(JPATH_SITE. "/images/com_folio/");
+			JFolder::create(JPATH_SITE."/images/com_folio/");
 		};
-		if(!file_exists(JPATH_SITE. "/images/com_folio/index.html"))
+		if(!file_exists(JPATH_SITE."/images/com_folio/index.html"))
 		{
-			JFile::copy(JPATH_SITE."/media/com_folio/index.html",JPATH_SITE. "/images/com_folio/index.html");
+			JFile::copy(JPATH_SITE."/media/com_folio/index.html",JPATH_SITE."/images/com_folio/index.html");
 		};
-		if(!file_exists(JPATH_SITE. "/images/com_folio/chocolate.png"))
+		if(!file_exists(JPATH_SITE."/images/com_folio/chocolate.png"))
 		{
-			JFile::copy(JPATH_SITE."/media/com_folio/images/chocolate. png",JPATH_SITE. "/images/com_folio/chocolate.png");
+			JFile::copy(JPATH_SITE."/media/com_folio/images/chocolate.png",JPATH_SITE."/images/com_folio/chocolate.png");
 		};
-		if(!file_exists(JPATH_SITE. "/images/com_folio/moneda.png"))
+		if(!file_exists(JPATH_SITE."/images/com_folio/moneda.png"))
 		{
-			JFile::copy(JPATH_SITE."/media/com_folio/images/moneda. png",JPATH_SITE. "/images/com_folio/moneda.png");
+			JFile::copy(JPATH_SITE."/media/com_folio/images/moneda.png",JPATH_SITE."/images/com_folio/moneda.png");
 		};
-		if(!file_exists(JPATH_SITE. "/images/com_folio/galleta.png"))
+		if(!file_exists(JPATH_SITE."/images/com_folio/galleta.png"))
 		{
-			JFile::copy(JPATH_SITE."/media/com_folio/images/glleta. png",JPATH_SITE. "/images/com_folio/galleta.png");
+			JFile::copy(JPATH_SITE."/media/com_folio/images/galleta.png",JPATH_SITE."/images/com_folio/galleta.png");
 		};
 	}
+
 	function install($parent)
 	{
 		$this->createSampleImages();
 		$parent->getParent()->setRedirectURL('index.php?option=com_folio');
 	}
+
 	function uninstall($parent)
 	{
-		echo '<p>' . JText::_('COM_FOLIO_UNINSTALL_TEXT') . '</p>';
+		echo '<p>'.JText::_('COM_FOLIO_UNINSTALL_TEXT').'</p>';
 	}
+
 	function update($parent)
 	{
 		$this->createSampleImages();
-		echo '<p>' . JText::_('COM_FOLIO_UPDATE_TEXT') . '</p>';
+		echo '<p>'.JText::_('COM_FOLIO_UPDATE_TEXT').'</p>';
 	}
+
 	function preflight($type, $parent)
 	{
-		echo '<p>' . JText::_('COM_FOLIO_PREFLIGHT_' . $type .'_TEXT') . '</p>';
+		echo '<p>'.JText::_('COM_FOLIO_PREFLIGHT_'.$type.'_TEXT').'</p>';
 	}
+
 	function postflight($type, $parent)
 	{
-		echo '<p>' . JText::_('COM_FOLIO_POSTFLIGHT_' . $type .'_TEXT') . '</p>';
+		echo '<p>'.JText::_('COM_FOLIO_POSTFLIGHT_'.$type.'_TEXT').'</p>';
 	}
 }
