70db9c2d4be04d6bfa8516b783cb9a56fb52a175
Nibaldo Barrera <nibaldobarrera@kazeta.cl>
2015-02-17 03:42:03 -0300

    V2.3.2 Dimensiones variables de ventana
    Se puede modificar ancho y alto de Ventana Emergente o Modal en
    parámetros de opciones

--- site/views/folios/tmpl/default.php
+++ site/views/folios/tmpl/default.php
@@ -1,7 +1,8 @@
 <?php
 defined('_JEXEC') or die;
-$width="400";
-$height="400";
+$width=$this->params->get('targetwidth');
+$height=$this->params->get('targetheight');
+$imagewidth=$this->params->get('imagewidth'); 
 ?>
 
 <div class="mypreview">
@@ -16,20 +17,20 @@ $height="400";
 			{
 				case 1:
 					// open in a new window
-					echo '<a href="'. $item->url .'" target="_blank" rel="nofollow">'.'<img src="'. $item->image .'" width="150"></a>';
+					echo '<a href="'. $item->url .'" target="_blank" rel="nofollow">'.'<img src="'. $item->image .'" width="'.$imagewidth.'"></a>';
 					break;
 				case 2:
 					// open in a popup window
 					$attribs = 'toolbar=no,location=no,status =no,menubar=no,scrollbars=yes,resizable=yes,width='.$this->escape($width).',height='.$this->escape($height).'';
 					echo "<a href=\"$item->url\" onclick=\"window.open(this.href, 'targetWindow', '".$attribs."'); return false;\">".
-					'<img src="'. $item->image .'" width="150"></a>';
+					'<img src="'. $item->image .'" width="'.$imagewidth.'"></a>';
 					break;
 				case 3:
 					// open in a modal window
 					JHtml::_('behavior.modal', 'a.modal'); ?>
 					<a class="modal" href="<?php echo $item->url;?>"
 					rel="{handler: 'iframe', size: {x:<?php echo $this->escape($width);?>, y:<?php echo $this->escape($height);?>}}">
-					<img src="<?php echo $item->image; ?>" width="150"></a>
+					<img src="<?php echo $item->image; ?>" width="'.$imagewidth.'"></a>
 					<?php
 					break;
 				default:
